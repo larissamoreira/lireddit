551847b3abeb9944c655e2b780210983472acb00
Larissa Gusmao <larissamgusmao@gmail.com>
2020-11-11 16:10:49 -0300

    apollo server and graphql setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,31 @@
+import "reflect-metadata";
+import { PostResolver } from './resolvers/post';
+import { HelloResolver } from './resolvers/hello';
 import { MikroORM } from '@mikro-orm/core';
-import { Post } from './entities/Post';
 import microConfig from './mikro-orm.config';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
 
-  //creating a post
-  // const post = orm.em.create(Post, {title: 'my first post'});
-  // await orm.em.persistAndFlush(post);
+  const app = express();
 
-  //fetching posts
-  // const posts = await orm.em.find(Post, {});
-  // console.log(posts);
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em })
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log('server started on localhost:4000')
+  })
 }
 
 main();
