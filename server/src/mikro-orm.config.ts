f745bf4ec13f2ad59aea58a3a83f0b2b93659ed7
Larissa Gusmao <larissamgusmao@gmail.com>
2020-11-12 13:12:10 -0300

    register and login resolvers

--- server/src/mikro-orm.config.ts
+++ server/src/mikro-orm.config.ts
@@ -1,6 +1,7 @@
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
 import { Post } from './entities/Post';
+import { User } from './entities/User';
 import path from 'path';
 
 export default {
@@ -8,7 +9,7 @@ export default {
     path: path.join(__dirname, './migrations'),
     pattern: /^[\w-]+\d+\.[tj]s$/,
   },
-  entities: [Post],
+  entities: [Post, User],
   dbName: 'lireddit',
   type: 'postgresql',
   user: 'postgres',
