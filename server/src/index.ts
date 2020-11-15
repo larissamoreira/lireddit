804b9707b6131ae8ff55aba33f0469a3ecd0b8e3
Larissa Gusmao <larissamgusmao@gmail.com>
2020-11-15 17:26:55 -0300

    expression-session

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,6 @@
 import "reflect-metadata";
+import { MyContext } from './types';
+import { __prod__ } from './constants';
 import { PostResolver } from './resolvers/post';
 import { HelloResolver } from './resolvers/hello';
 import { MikroORM } from '@mikro-orm/core';
@@ -7,6 +9,9 @@ import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import { UserResolver } from "./resolvers/user";
+import redis from 'redis';
+import connectRedis from 'connect-redis';
+import session from "express-session";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
@@ -14,19 +19,43 @@ const main = async () => {
 
   const app = express();
 
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+  
+  app.use(
+    session({
+      name: 'qid',
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 20, // 10 years
+        httpOnly: true,
+        sameSite: 'lax', //csrf
+        secure: !__prod__ // cookie only works in https
+      },
+      saveUninitialized: false,
+      secret: 'ahuehuahwe',
+      resave: false,
+    })
+  )
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
-    context: () => ({ em: orm.em })
+    context: ({ req, res }): MyContext => ({ em: orm.em, req, res })
   });
 
   apolloServer.applyMiddleware({ app });
 
   app.listen(4000, () => {
-    console.log('server started on localhost:4000')
-  })
-}
+    console.log('server started on localhost:4000');
+  });
+};
 
-main();
+main().catch((err) => {
+  console.log(err);
+});
--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -1,6 +1,6 @@
 import { User } from './../entities/User';
 import { MyContext } from './../types';
-import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Resolver } from "type-graphql";
+import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Query, Resolver } from "type-graphql";
 import argon2 from 'argon2';
 
 @InputType()
@@ -30,10 +30,19 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+  @Query(() => User, { nullable: true })
+  async me(@Ctx() { req, em }: MyContext) {
+    if (!req.session.userId) {
+      return null; //user not logged in
+    }
+    const user = em.findOne(User, { id: req.session.userId });
+    return user;
+  }
+
   @Mutation(() => UserResponse)
   async register(
     @Arg('options') options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext
+    @Ctx() { em, req }: MyContext
   ): Promise<UserResponse> {
     if (options.username.length <= 2) {
       return {
@@ -71,13 +80,16 @@ export class UserResolver {
         }
       }
     }
+
+    req.session.userId = user.id;
+
     return { user };
   }
 
   @Mutation(() => UserResponse)
   async login(
     @Arg('options') options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext
+    @Ctx() { em, req }: MyContext
   ): Promise<UserResponse> {
     const user = await em.findOne(User, { username: options.username });
     if (!user) {
@@ -97,6 +109,8 @@ export class UserResolver {
         }]
       }
     };
+
+    req.session.userId = user.id;
 
     return { user };
   }
